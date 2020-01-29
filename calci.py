01db4ba4ed255bff83ad529996fe3cca252796b9
kaviyavenkat01 <60418730+kaviyavenkat01@users.noreply.github.com>
2020-01-29 10:58:17 +0530

    Update calci.py

--- calci.py
+++ calci.py
@@ -1,1 +1,16 @@
-
+def add(x,y)
+       return x+y
+def sub(x,y)
+       return x-y
+def mul(x,y)
+       return x*y
+def div(x,y)
+       return x%y
+print("select operation")
+print("1.add")
+print("2.sub")
+print("3.mul")
+print("4.div")
+add=int(input("enter the number:"))
+    
+    
