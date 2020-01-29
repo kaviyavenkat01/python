4d54304781ec46ca8edeeff22ab3dd8d40429f50
kaviyavenkat01 <60418730+kaviyavenkat01@users.noreply.github.com>
2020-01-29 12:36:11 +0530

    Create bill.py

---
+++ bill.py
@@ -0,0 +1,6 @@
+name=int(input("enter the name:"))
+amount=int(input("enter the amount:"))
+km=int(input("enter the km:"))
+place=int(input("enter the locaion:"))
+amount=distance*km;
+print("amount spent:")
